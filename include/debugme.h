f38c63244a86a4c9396bfcd1be0d7616da9b3c17
Yury Gribov <tetra2005@gmail.com>
2016-01-19 01:23:42 +0300

    Remove thread safety comment.

--- include/debugme.h
+++ include/debugme.h
@@ -5,9 +5,8 @@ enum {
   DEBUGME_XTERM = 1 << 0,
 };
 
+// These APIs are not thread-safe.
 int debugme_install_sighandlers(unsigned flags, const char *dbg_opts);
-
-// Thread/signal-safe
 int debugme_debug(unsigned flags, const char *dbg_opts);
 
 #endif
