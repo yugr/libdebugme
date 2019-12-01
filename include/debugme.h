c7aa0d62b3af32470bc7a0d5709a100a0492fa9c
Yury Gribov <tetra2005@gmail.com>
2019-12-01 18:50:39 +0300

    Update copyright.

--- include/debugme.h
+++ include/debugme.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Yury Gribov
+ * Copyright 2016-2018 Yury Gribov
  * 
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE.txt file.
