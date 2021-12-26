c712385df80d0a785aca2377a2dc5b6f4ee2e89f
Yuri Gribov <tetra2005@gmail.com>
2021-12-26 11:10:26 +0300

    First take on GH actions.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Copyright 2019 Yury Gribov
+# Copyright 2019-2021 Yury Gribov
 # 
 # Use of this source code is governed by MIT license that can be
 # found in the LICENSE.txt file.
 
 set -eu
 
-if test -n "${TRAVIS:-}"; then
+if test -n "${TRAVIS:-}" -o -n "${GITHUB_ACTIONS:-}"; then
   set -x
 fi
 
