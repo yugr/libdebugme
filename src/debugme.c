34008005376e54d80e3f31136a01e5f8785588ab
Yuri Gribov <tetra2005@gmail.com>
2022-01-09 16:46:07 +0300

    [skip ci] Update copyrights.

--- src/debugme.c
+++ src/debugme.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 Yury Gribov
+ * Copyright 2016-2022 Yury Gribov
  * 
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE.txt file.
